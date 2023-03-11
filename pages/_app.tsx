a2723787be7340a91b1e2ec4dbd576a11f7b9d5b
Paul Mendrik <paul@imouse.net>
2023-03-11 14:25:24 +0100

    updated font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,7 @@
 import '../styles/app.scss'
-import { Barlow } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AnimatePresence } from "framer-motion"
-
-const barlow = Barlow({
-  weight: ['200','300','400','500','900'],
-  subsets: ['latin'],
-  variable: '--barlow-font',
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   
